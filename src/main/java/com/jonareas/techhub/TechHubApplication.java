edd57f1de59d9dc0807b66d14df720db56b64dea
jxareas <areasjx@gmail.com>
2022-05-10 17:36:01 -0600

    Init Spring Boot Template

---
+++ src/main/java/com/jonareas/techhub/TechHubApplication.java
@@ -0,0 +1,13 @@
+package com.jonareas.techhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechHubApplication.class, args);
+    }
+
+}
